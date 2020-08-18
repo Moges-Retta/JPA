5b48bb5313282f72766c73c4184d0cfe13123dad
Moges <Moges.ash@gmail.com>
2020-08-18 18:06:54 +0200

    JpaDocentRepository test

---
+++ src/main/java/be/vdab/fietsacademy/domain/Docent.java
@@ -0,0 +1,57 @@
+package be.vdab.fietsacademy.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "docenten")
+public class Docent {
+    @Id
+    private long id;
+    private String voornaam;
+    private String familienaam;
+    private BigDecimal wedde;
+    private String emailAdres;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getVoornaam() {
+        return voornaam;
+    }
+
+    public void setVoornaam(String voornaam) {
+        this.voornaam = voornaam;
+    }
+
+    public String getFamilienaam() {
+        return familienaam;
+    }
+
+    public void setFamilienaam(String familienaam) {
+        this.familienaam = familienaam;
+    }
+
+    public BigDecimal getWedde() {
+        return wedde;
+    }
+
+    public void setWedde(BigDecimal wedde) {
+        this.wedde = wedde;
+    }
+
+    public String getEmailAdres() {
+        return emailAdres;
+    }
+
+    public void setEmailAdres(String emailAdres) {
+        this.emailAdres = emailAdres;
+    }
+}
